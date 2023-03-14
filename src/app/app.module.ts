d3cdeb19d590d59b434e297b349eb5bbcff3438c
Katherine Picazo <kpicazo@kruxanalytics.com>
2023-03-13 19:09:54 -0600

    Use two-way binding to populate filter input element

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
+  // directives and components declared in this app
   declarations: [
     AppComponent,
     ProductListComponent
   ],
+  // directives and components from Angular itself, or from other sources (e.g. third-party libraries)
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   bootstrap: [AppComponent]
 })
