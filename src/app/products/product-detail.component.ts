f4a2994f7a3be211c1390c3bed20cc7da9a62a3d
Katherine Picazo <kpicazo@kruxanalytics.com>
2023-03-14 14:40:59 -0600

    Add Back button and activate route with code

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct } from './product';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -7,11 +8,30 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
+  product: IProduct | undefined;
 
-  constructor(private route: ActivatedRoute ) { }
+  constructor(private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit(): void { 
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.pageTitle += `: ${id}`
+
+    // imitate an HTTP response by hardcoding product data
+    this.product = {
+      "productId": 10,
+      "productName": "Video Game Controller",
+      "productCode": "GMG-0042",
+      "releaseDate": "October 15, 2020",
+      "description": "Standard two-button video game controller",
+      "price": 35.95,
+      "starRating": 4.6,
+      "imageUrl": "assets/images/xbox-controller.png"
+    }
+  }
+
+  // Routing with code (vs routing with routerlink directive in a template)
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 }
