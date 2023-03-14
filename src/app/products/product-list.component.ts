23253b7ea00def28c68651424f3cf4104902d648
Katherine Picazo <kpicazo@kruxanalytics.com>
2023-03-13 19:01:17 -0600

    Add event handler to image toggle button

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -8,6 +8,7 @@ export class ProductListComponent {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
+    showImage: boolean = false;
     products: any[] = [
         {
             "productId": 1,
@@ -29,5 +30,11 @@ export class ProductListComponent {
             "starRating": 4.2,
             "imageUrl": "assets/images/garden_cart.png"
         }
-    ]
+    ];
+
+    // By convention, class methods are created after its properties are defined.
+    // We don't need the "function" keyword here
+    toggleImage() : void {
+        this.showImage = !this.showImage;
+    }
  }
